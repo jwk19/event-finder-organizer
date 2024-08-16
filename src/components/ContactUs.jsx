215e3a9802ce56bd60e95d47ccfe8101b092b45c
tony-g30 <tonyaudo@gmail.com>
2024-08-16 07:02:38 +0300

    adding contact page

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Styles/ContactUs.css';
+
+function ContactUs() {
+  return (
+    <div className="contact-us">
+      <h2>Contact Us</h2>
+      <p>Email: support@eventfinder.com</p>
+      <p>Phone: +123 456 7890</p>
+    </div>
+  );
+}
+
+export default ContactUs;

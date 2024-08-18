089432b1662633350d7255a09174ea3a29737187
abdal farah <abdalla2024farah>
2024-08-18 20:55:17 +0300

    contactus added

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import '../Styles/ContactUs.css';
+import './ContactUs.css'; 
 
-function ContactUs() {
+const ContactUs = () => {
+  const developers = [
+    { name: 'Abdalla farah', email: 'abdalla999farah@gmail.com' },
+    { name: 'cindy akumi', email: 'cindy.akumu@student.moringaschool.com' },
+    { name: 'tony udo', email: 'tonyaudo@gmail.com' },
+    { name: 'james kahwa', email: 'viralwebdigital@gmail.com' },
+
+  ];
+
   return (
     <div className="contact-us">
-      <h2>Contact Us</h2>
-      <p>Email: support@eventfinder.com</p>
-      <p>Phone: +123 456 7890</p>
+      <h1>Contact Us</h1>
+      <p>If you have any questions or feedback, please reach out to our developers,Thank you:</p>
+      <ul>
+        {developers.map((dev, index) => (
+          <li key={index}>
+            <strong>{dev.name}:</strong> <a href={`mailto:${dev.email}`}>{dev.email}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default ContactUs;
